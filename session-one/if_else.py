3956b039f743cf487edb70f3db11f6d5b8d04999
BFC-ChrisGreen <134616254+BFC-ChrisGreen@users.noreply.github.com>
2025-06-16 10:43:21 +0100

    Create if_else.py

---
+++ session-one/if_else.py
@@ -0,0 +1,14 @@
+# Task 1: Write an if-else statement that checks if a number is positive or negative.
+
+
+# Task 2: Write an if-elif-else statement that checks if a number is:
+# - less than 10
+# - equal to 10
+# - greater than 10
+# Your code here:
+
+
+# Task 3: Write an if-else statement that checks if a person is old enough to vote.
+# Assume the voting age is 18 and the person's age is 16.
+# Your code here:
+
