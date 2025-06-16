88339cec7467b1171b1ac9d7c2a188262b097b70
BFC-ChrisGreen <134616254+BFC-ChrisGreen@users.noreply.github.com>
2025-06-16 10:42:19 +0100

    Added for_loops.py file

---
+++ session-one/for_loops.py
@@ -0,0 +1,9 @@
+# Task 1: Use a for loop to print numbers from 1 to 5.
+
+
+# Task 2: Use a for loop to print each character in the string "Python".
+
+
+# Task 3: Use a for loop to print the square of numbers from 1 to 5.
+
+
